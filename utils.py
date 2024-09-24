c10c696460065f66625a65a5567adfd98ee21ec2
ifyyy10 <75942656+ifyyy10@users.noreply.github.com>
2024-09-24 21:43:12 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -39,40 +39,39 @@ trainId_to_name = {
 
 
 color_to_trainId = {
-    (  0,  0,  0): 0,    # unlabeled
-    (  0,  0,  0): 1,    # ego vehicle
-    (  0,  0,  0): 2,    # rectification border
-    (  0,  0,  0): 3,    # out of roi
-    (  0,  0,  0): 4,    # static
-    (111, 74,  0): 5,    # dynamic
-    ( 81,  0, 81): 6,    # ground
-    (128, 64,128): 7,    # road
-    (244, 35,232): 8,    # sidewalk
-    (250,170,160): 9,    # parking
-    (230,150,140): 10,   # rail track
-    ( 70, 70, 70): 11,   # building
-    (102,102,156): 12,   # wall
-    (190,153,153): 13,   # fence
-    (180,165,180): 14,   # guard rail
-    (150,100,100): 15,   # bridge
-    (150,120, 90): 16,   # tunnel
-    (153,153,153): 17,   # pole
-    (153,153,153): 18,   # polegroup
-    (250,170, 30): 19,   # traffic light
-    (220,220,  0): 20,   # traffic sign
-    (107,142, 35): 21,   # vegetation
-    (152,251,152): 22,   # terrain
-    ( 70,130,180): 23,   # sky
-    (220, 20, 60): 24,   # person
-    (255,  0,  0): 25,   # rider
-    (  0,  0,142): 26,   # car
-    (  0,  0, 70): 27,   # truck
-    (  0, 60,100): 28,   # bus
-    (  0,  0, 90): 29,   # caravan
-    (  0,  0,110): 30,   # trailer
-    (  0, 80,100): 31,   # train
-    (  0,  0,230): 32,   # motorcycle
-    (119, 11, 32): 33,   # bicycle
-    (  0,  0,142): -1,   # license plate
+    (0, 0, 0): 255,    # unlabeled
+    (0, 0, 0): 255,    # ego vehicle
+    (0, 0, 0): 255,    # rectification border
+    (0, 0, 0): 255,    # out of roi
+    (0, 0, 0): 255,    # static
+    (111, 74, 0): 255,    # dynamic
+    (81, 0, 81): 255,    # ground
+    (128, 64, 128): 0,    # road
+    (244, 35, 232): 1,    # sidewalk
+    (250, 170, 160): 255,   # parking
+    (230, 150, 140): 255,   # rail track
+    (70, 70, 70): 2,   # building
+    (102, 102, 156): 3,   # wall
+    (190, 153, 153): 4,   # fence
+    (180, 165, 180): 255,   # guard rail
+    (150, 100, 100): 255,   # bridge
+    (150, 120, 90): 255,   # tunnel
+    (153, 153, 153): 5,   # pole
+    (153, 153, 153): 255,   # polegroup
+    (250, 170, 30): 6,   # traffic light
+    (220, 220, 0): 7,   # traffic sign
+    (107, 142, 35): 8,   # vegetation
+    (152, 251, 152): 9,   # terrain
+    (70, 130, 180): 10,   # sky
+    (220, 20, 60): 11,   # person
+    (255, 0, 0): 12,   # rider
+    (0, 0, 142): 13,   # car
+    (0, 0, 70): 14,   # truck
+    (0, 60, 100): 15,   # bus
+    (0, 0, 90): 255,   # caravan
+    (0, 0, 110): 255,   # trailer
+    (0, 80, 100): 16,   # train
+    (0, 0, 230): 17,   # motorcycle
+    (119, 11, 32): 18,   # bicycle
+    (0, 0, 142): -1,   # license plate
 }
-
